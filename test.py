f0b5b69337fc1868a86820343c7057519be7be7c
Geethanjali313 <87926222+Geethanjali313@users.noreply.github.com>
2021-07-26 16:00:57 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,68 @@
+import speech_recognition as sr
+import pywhatkit
+import pyttsx3
+import datetime
+import wikipedia
+import pyjokes
+
+listener=sr.Recognizer()
+engine=pyttsx3.init()
+voices=engine.getProperty('voices')
+engine.setProperty('voice',voices[1].id)
+
+def talk(text):
+    engine.say(text)
+    engine.runAndWait()
+
+def take_command():
+    try:
+         with sr.Microphone() as source:
+             print('listening...')
+             voice = listener.listen(source)
+             command = listener.recognize_google(voice)
+             command = command.lower()
+             if 'alexa' in command:
+                 command=command.replace('alexa','')
+                 print(command)
+
+    except:
+         pass
+    return command
+
+def run_alexa():
+    command=take_command()
+    print(command)
+    if 'play' in command:
+        song=command.replace('play','')
+        talk('playing'+song)
+        pywhatkit.playonyt(song)
+
+    elif 'time' in command:
+        time=datetime.datetime.now().strftime('%I:%M %p')
+        print(time)
+        talk('current time is'+time)
+
+    elif 'wikipedia' in command:
+        person=command.replace('wikipedia','')
+        info=wikipedia.summary(person,1)
+        print(info)
+        talk(info)
+
+    elif 'date' in command:
+        talk('sorry,I have a headache')
+
+    elif 'are you single' in command:
+        talk('I am in a relationship with wifi')
+
+    elif 'jokes' in command:
+        talk(pyjokes.get_joke())
+
+    elif 'do you like cricket' in command:
+        talk('yes, I like only RCB jai RCB e sala cup namde')
+
+    else:
+        talk('Please say the command again')
+
+while True:
+    run_alexa()
+
